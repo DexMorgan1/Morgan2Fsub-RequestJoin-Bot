a8c1dff4a4075b6723a3950f0435c8550f06e29d
DexMorgan1 <147988607+DexMorgan1@users.noreply.github.com>
2026-09-04 11:33:32 -0700

    Add retry emoji to force-subscribe button

--- plugins/start.py
+++ plugins/start.py
@@ -113,7 +113,7 @@ async def not_joined(client: Client, message: Message):
 
     try:
         buttons.append([InlineKeyboardButton(
-            text="Try Again",
+            text="🔄 Try Again",
             url=f"https://t.me/{client.username}?start={message.command[1]}",
         )])
     except IndexError:
